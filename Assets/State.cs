74cabfcae2c78530a8e4a4717c3107c615eb9343
PhoenixV01D <123003885+PhoenixV01D@users.noreply.github.com>
2023-02-12 20:28:15 -0600

    Add files via upload

---
+++ Assets/State.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[CreateAssetMenu(menuName = "State")]
+public class State : ScriptableObject
+{
+    [SerializeField] string gameText;
+    [SerializeField] State[] otherStates;
+
+    // return the game text for the state
+    public string GetStateText()
+    {
+        return gameText;
+    }
+
+    // Update is called once per frame
+    public State[] GetOtherStates() 
+    {
+        return otherStates;
+    }
+}
